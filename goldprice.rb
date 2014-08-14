ae45cb9daad33672b138132e43705bc2646fdb83
neillsilva <blackvector@gmail.com>
2014-08-14 18:24:49 -0400

    Ruby practices.

---
+++ goldprice.rb
@@ -0,0 +1,5 @@
+price_per_ounce = 1336
+
+def price_in_gold(ounces)
+	ounces * price_per_ounce
+end
---
+++ math_week_2.rb
@@ -0,0 +1,15 @@
+puts
+puts "************"
+puts 4 * 4
+puts 5 + 5
+puts 4/4
+puts
+puts "4 ** 4 results in:"
+puts 4**4
+puts "Hint: It's 4 to the 4th power."
+puts "3/4 results in:"
+puts 3/4
+puts "3/4.0 results in:"
+puts 3/4.0 
+puts "Hint: 3/4.0 is a float, while 3/4 is an interger."
+puts "************"
