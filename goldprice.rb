605fe7085849a45c2a35380a7a731e99569afe65
neillsilva <blackvector@gmail.com>
2014-08-14 18:28:51 -0400

    Edit files.

--- goldprice.rb
+++ goldprice.rb
@@ -1,5 +1,10 @@
 price_per_ounce = 1336
 
-def price_in_gold(ounces)
+def price_of_gold(ounces)
 	ounces * price_per_ounce
 end
+
+def price_of_gold_from_pounds(pounds)
+	(pounds * 16) * price_per_ounce
+end
+
