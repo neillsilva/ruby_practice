0266e0601131d4e56efc54db798571e215b767ba
neillsilva <blackvector@gmail.com>
2014-08-28 20:36:32 -0400

    Arrays

---
+++ assessment_two.rb
@@ -0,0 +1,23 @@
+ar = [1,2,3,4]
+v1 = [1,2,3,4]
+v2 = ["a", "b", "c", "d"]
+
+puts ar.map { |a| a+3 }
+
+ar.each do |num|
+	puts num += 3
+end
+
+# Print all capital letters
+puts ('A'..'Z').each { |letter| puts letter }
+
+# Cross Product Arrays
+def cross_product v1, v2
+	out = []
+	v1.each do | i |
+		v2.each do | j |
+			out << ( i + j ) 
+		end
+	end
+	out 
+end
