8ec15496d3f9faeecc8037bfa231cc1e18217a3a
neillsilva <blackvector@gmail.com>
2014-09-04 07:01:28 -0400

    Fixing \n problem

--- week5_assessment/anagrams.rb
+++ week5_assessment/anagrams.rb
@@ -1,9 +1,18 @@
 words = []
 
 words = IO.readlines('dictionary.txt')
+words.map! { |x| x.chomp}
+# Using this escapes the problem below of the last line not having \n! Yay!
+
 words.group_by { |element| element.downcase.chars.sort }.values
 # The above works great, from what I can tell. Thanks for giving us an 80k word dictionary Justin.
 # (Hint: You can just test with a custom made smaller dictionary to make sure this works.)
+###!!!###
+# OK, problem. When trying with a smaller dictionary file I realized that \n is 
+# causing the last line in the dictionary to not apply (if scream is 2nd to last and 
+# creams is last they will not end up in the same hash because of \n showing in hash)
+###!!!###
+
 
 # Now I have to figure out how to sort this hash to show which key has the most anagrams. TO GOOGLE!
 
