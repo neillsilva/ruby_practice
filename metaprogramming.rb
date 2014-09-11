b9e15a4fadd94b3bd486b73e6738e1e7e9dc5efb
neillsilva <blackvector@gmail.com>
2014-09-11 18:50:33 -0400

    Constant capitalize

--- metaprogramming.rb
+++ metaprogramming.rb
@@ -8,7 +8,7 @@ end
 
 class DogFactory < Dog
 	def self.create_class(class_name)	
-		klass ||= Object.const_set(class_name, Class.new(Dog))
+		klass ||= Object.const_set(class_name.capitalize, Class.new(Dog))
 	end
 
 end
